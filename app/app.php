0ec828f9204d3fc17054d3bafc9409b7a19d325c
Dan Lauby <dmlauby@gmail.com>
2017-02-16 14:48:35 -0800

    Create a score twig template to display word score

--- app/app.php
+++ app/app.php
@@ -18,5 +18,13 @@
     $app->get('/', function() use ($app) {
         return $app['twig']->render('play_game.html.twig');
     });
+
+    $app->post('/word', function() use ($app) {
+        $input_word = $_POST['word'];
+        $new_game = new Game($input_word);
+        $word = $new_game->getWord();
+        $score = $new_game->compareLetters($input_word);
+        return $app['twig']->render('score.html.twig', array('score' => $score, 'word' => $word));
+    });
     return $app;
 ?>
